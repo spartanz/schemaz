880da39261e6eefdb27566ac1c709e72ff700de1
insdami <Damian Albrun damian.ea@gmail.com>
2018-08-13 07:54:07 +0100

    Removed 'A' values from 'JsonPrim'

--- src/main/scala/schemas.scala
+++ src/main/scala/schemas.scala
@@ -7,11 +7,11 @@ object ScalaSchema {
 }
 
 object JsonSchema {
-  type Prim[A] = JsonPrim[A]
+  type Prim[A] = JsonPrim
 
-  sealed trait JsonPrim[A]
-  final case class JsonString(value: String) extends JsonPrim[String]
-  final case class JsonNumber(value: BigDecimal) extends JsonPrim[BigDecimal]
-  final case class JsonBool(value: Boolean) extends JsonPrim[Boolean]
-  case object JsonNull extends JsonPrim[Nothing]
+  sealed trait JsonPrim
+  case object JsonString extends JsonPrim
+  case object JsonNumber extends JsonPrim
+  case object JsonBool extends JsonPrim
+  case object JsonNull extends JsonPrim
 }
