7f3c4f2e5c39574177ce25bbaf6437599df2eeee
Graf_Blutwurst <dominicegger@bluewin.ch>
2018-12-12 17:32:22 +0100

    fixed compile errors in GenModule

--- modules/scalacheck/src/main/scala/GenModule.scala
+++ modules/scalacheck/src/main/scala/GenModule.scala
@@ -5,6 +5,7 @@ package schema
 package scalacheck
 
 import org.scalacheck.Gen
+
 import GenModule._
 
 trait GenModule extends SchemaModule {
@@ -22,54 +23,32 @@ trait GenModule extends SchemaModule {
   private def schemaToGen(implicit primToGen: Prim ~> Gen): Schema ~> Gen =
     new (Schema ~> Gen) {
       override def apply[A](schema: Schema[A]): Gen[A] = schema match {
-        case prim: Schema.PrimSchema[_]     => primToGen(prim.prim)
-        case record: Schema.RecordSchema[_] => recordGen(record)
-        case union: Schema.Union[_, _]      => unionGen(union)
-        case seq: Schema.SeqSchema[_]       => seqGen(seq)
-        case iso: Schema.IsoSchema[_, _]    => schemaToGen(primToGen)(iso.base).map(iso.iso.get)
+        case prim: Schema.PrimSchema[_]        => primToGen(prim.prim)
+        case record: Schema.RecordSchema[_, _] => recordGen(record)
+        case union: Schema.Union[_, _]         => unionGen(union)
+        case seq: Schema.SeqSchema[_]          => seqGen(seq)
+        case iso: Schema.IsoSchema[_, _]       => schemaToGen(primToGen)(iso.base).map(iso.iso.get)
+        case opt: Schema.OptionalSchema[_]     => Gen.option(schemaToGen(primToGen)(opt.base))
       }
     }
 
-  private def recordGen[A](
-    schema: Schema.RecordSchema[A]
+  def nt[ID, A](implicit primToGen: Prim ~> Gen): (Schema.Term[ID, A, ?] ~> Gen[?]) =
+    new (Schema.Term[ID, A, ?] ~> Gen[?]) {
+      override def apply[X](branch: Schema.Term[ID, A, X]): Gen[X] =
+        schemaToGen(primToGen)(branch.base)
+    }
+
+  private def recordGen[A, AP](
+    schema: Schema.RecordSchema[A, AP]
   )(
     implicit
     primToGen: Prim ~> Gen
-  ): Gen[A] = {
-    implicit val genAp: Applicative[Gen] = new Applicative[Gen] {
-      override def ap[T, U](fa: => Gen[T])(f: => Gen[T => U]): Gen[U] =
-        fa.flatMap(a => f.map(_(a)))
-      override def point[T](a: => T): Gen[T] = Gen.const(a)
-    }
-
-    schema.fields.foldMap(new (Schema.Field[A, ?] ~> Gen) {
-      override def apply[B](fa: Schema.Field[A, B]): Gen[B] = fa match {
-        case Schema.Field.Essential(_, base, _, _) =>
-          schemaToGen(primToGen)(base)
-        case Schema.Field.NonEssential(_, base, _) =>
-          Gen.option(schemaToGen(primToGen)(base))
-      }
-    })
-  }
+  ): Gen[A] = FreeAp2.covariantFold(schema.fields)(nt[ProductTermId, A]).map(schema.g)
 
   private def unionGen[A, AE](
     schema: Schema.Union[A, AE]
-  )(implicit primToGen: Prim ~> Gen): Gen[A] = {
-    val nt: (Schema.Branch[A, ?] ~> Gen[?]) = new (Schema.Branch[A, ?] ~> Gen[?]) {
-      override def apply[X](branch: Schema.Branch[A, X]): Gen[X] = branchGen(branch)
-    }
-
-    val eGen = FreeChoice.fold(schema.choices)(nt)
-    eGen.map(schema.g)
-  }
-
-  private def branchGen[A, A0](
-    branch: Schema.Branch[A, A0]
-  )(
-    implicit
-    primToGen: Prim ~> Gen
-  ): Gen[A0] =
-    schemaToGen(primToGen)(branch.base)
+  )(implicit primToGen: Prim ~> Gen): Gen[A] =
+    FreeChoice.covariantFold(schema.choices)(nt[SumTermId, A]).map(schema.g)
 
   private def seqGen[A](
     schema: Schema.SeqSchema[A]
@@ -81,13 +60,24 @@ trait GenModule extends SchemaModule {
 }
 
 object GenModule {
-  implicit val genThing: Alt[Gen] = new Alt[Gen] {
 
-    def alt[A](fa:Gen[A],fb:Gen[A]):Gen[A] = for {
-      a <- fa
-      b <- fb
-      x <- Gen.oneOf(a,b)
-    } yield x
+  implicit val genAp: Applicative[Gen] = new Applicative[Gen] {
+    override def ap[T, U](fa: => Gen[T])(f: => Gen[T => U]): Gen[U] =
+      fa.flatMap(a => f.map(_(a)))
+    override def point[T](a: => T): Gen[T] = Gen.const(a)
+  }
+
+  implicit val genAlt: Alt[Gen] = new Alt[Gen] {
+    override def point[A](a: => A): org.scalacheck.Gen[A] = Gen.const(a)
+    override def ap[A, B](fa: => org.scalacheck.Gen[A])(
+      f: => org.scalacheck.Gen[A => B]
+    ): org.scalacheck.Gen[B] = genAp.ap(fa)(f)
+    override def alt[X](fa: => Gen[X], fb: => Gen[X]): Gen[X] =
+      for {
+        a <- fa
+        b <- fb
+        x <- Gen.oneOf(a, b)
+      } yield x
   }
 
 }
