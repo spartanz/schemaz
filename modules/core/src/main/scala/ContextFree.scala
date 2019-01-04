12ea1e6c9982b961a0e6c4f22cf783029b93ccf6
Graf_Blutwurst <dominicegger@bluewin.ch>
2019-01-04 18:24:52 +0100

    changed to different style of pattern matching to work around weird type behaviour

--- modules/core/src/main/scala/ContextFree.scala
+++ modules/core/src/main/scala/ContextFree.scala
@@ -2,7 +2,6 @@ package scalaz
 
 package schema
 
-
 trait ContextFreeAlgebras extends SchemaModule {
 
   import Schema._
@@ -10,15 +9,16 @@ trait ContextFreeAlgebras extends SchemaModule {
   type Id[A] = A
 
   def covariantTargetFunctor[H[_]](
-    primNT: Prim ~> H, 
-    seqNT: H ~> λ[X => H[List[X]]], 
-    prodLabelNT: λ[X => H[(ProductTermId, X)]] ~> H, 
+    primNT: Prim ~> H,
+    seqNT: H ~> λ[X => H[List[X]]],
+    prodLabelNT: λ[X => H[(ProductTermId, X)]] ~> H,
     sumLabelNT: λ[X => H[(SumTermId, X)]] ~> H,
     one: H[Unit]
-  )(implicit H:Alt[H]) : HAlgebra[Schema, H] = new (Schema[H, ?] ~> H) {
+  )(implicit H: Alt[H]): HAlgebra[Schema, H] = new (Schema[H, ?] ~> H) {
+
     def apply[A](schema: Schema[H, A]): H[A] = schema match {
       case PrimSchema(prim)          => primNT(prim)
-      case :*:(left, right)          => H.tuple2(left,right)
+      case :*:(left, right)          => H.tuple2(left, right)
       case :+:(left, right)          => H.either2(left, right)
       case IsoSchema(base, iso)      => H.map(base)(iso.get)
       case RecordSchema(fields, iso) => H.map(fields)(iso.get)
@@ -30,25 +30,31 @@ trait ContextFreeAlgebras extends SchemaModule {
     }
   }
 
+
+
   def contravariantTargetFunctor[H[_]](
-    primNT: Prim ~> H, 
-    seqNT: H ~> λ[X => H[List[X]]], 
-    prodLabelNT: λ[X => H[(ProductTermId, X)]] ~> H, 
+    primNT: Prim ~> H,
+    seqNT: H ~> λ[X => H[List[X]]],
+    prodLabelNT: λ[X => H[(ProductTermId, X)]] ~> H,
     sumLabelNT: λ[X => H[(SumTermId, X)]] ~> H,
     pure: Id ~> H,
     one: H[Unit]
-  )(implicit H:Decidable[H]) : HAlgebra[Schema, H] = new (Schema[H, ?] ~> H) {
+  )(implicit H: Decidable[H]): HAlgebra[Schema, H] = new (Schema[H, ?] ~> H) {
+
     def apply[A](schema: Schema[H, A]): H[A] = schema match {
-      case PrimSchema(prim)          => primNT(prim)
-      case :*:(left, right)          => H.tuple2(left,right)
-      case x: :+:[_, a, b]           => H.choose(x.left,x.right)(identity[a \/ b])
-      //UHOH THOSE BOTH COMPILE?!
+      case PrimSchema(prim) => primNT(prim)
+      case :*:(left, right) => H.tuple2(left, right)
+      case x: :+:[_, a, b]  => H.choose(x.left, x.right)(identity[a \/ b])
+      //UHOH THOSE BOTH COMPILE?! (for the love of all that is precious to you, please leave the pattern matches that actually bind the type variables)
       //case IsoSchema(base, iso)      => H.contramap(base)(iso.get)
-      case IsoSchema(base, iso)      => H.contramap(base)(iso.reverseGet)
-      case RecordSchema(fields, iso) => H.contramap(fields)(iso.reverseGet)
+      //case IsoSchema(base, iso)      => H.contramap(base)(iso.reverseGet)
+      //Luckily does not compile
+      //case x: IsoSchema[_, a, a0]    => H.contramap(x.base)(x.iso.get)
+      case x: IsoSchema[_, a, a0]    => H.contramap(x.base)(x.iso.reverseGet)
+      case x: RecordSchema[_, a, a0] => H.contramap(x.fields)(x.iso.reverseGet)
       case SeqSchema(element)        => seqNT(element)
       case ProductTerm(id, base)     => prodLabelNT(H.tuple2(pure(id), base))
-      case Union(choices, iso)       => H.contramap(choices)(iso.reverseGet)
+      case x: Union[_, a, a0]       => H.contramap(x.choices)(x.iso.reverseGet)
       case SumTerm(id, base)         => sumLabelNT(H.tuple2(pure(id), base))
       case One()                     => one
     }
