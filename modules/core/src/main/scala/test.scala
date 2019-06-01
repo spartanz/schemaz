7aa9dfa58a776b4a9dcac6c7b6a5e384692cb647
Valentin Kasas <valentinkasas@MacBook-Pro-de-Valentin.local>
2019-06-01 09:56:05 -0600

    WIPer

--- modules/core/src/main/scala/test.scala
+++ modules/core/src/main/scala/test.scala
@@ -33,37 +33,69 @@ object module extends JsonModule[JsonSchema.type] with HasMigration[JsonSchema.t
     )
   val homer = Person("Homer Simpson", Some(User(active = true, username = "SpiderPig")))
 
+  trait ForAll[F[_], TC[_]] {
+    def apply[A: TC]: F[A]
+  }
+
   trait Lookup[Re, A] {
-    def apply(registry: Re): BareSchema[A]
+    def apply(registry: Re): Schema[A]
   }
 
   trait LowPrioLookup {
     implicit def rightLookup[R1, RT, A](implicit rest: Lookup[RT, A]): Lookup[(R1, RT), A] =
       new Lookup[(R1, RT), A] {
-        def apply(registry: (R1, RT)): BareSchema[A] = rest(registry._2)
+        def apply(registry: (R1, RT)): Schema[A] = rest(registry._2)
       }
   }
 
   object Lookup extends LowPrioLookup {
-    implicit def leftLookup[RR, A]: Lookup[(BareSchema[A], RR), A] =
-      new Lookup[(BareSchema[A], RR), A] {
-        def apply(registry: (BareSchema[A], RR)): BareSchema[A] = registry._1
+    implicit def leftLookup[RR, A]: Lookup[(Schema[A], RR), A] =
+      new Lookup[(Schema[A], RR), A] {
+        def apply(registry: (Schema[A], RR)): Schema[A] = registry._1
       }
   }
 
   class Registry[Re](registry: Re) { self =>
-    def lookup[A](implicit l: Lookup[Re, A]): BareSchema[A] = l(registry)
+    def lookup[A](implicit l: Lookup[Re, A]): Schema[A] = l(registry)
 
-    def addEntry[A](mkEntry: Registry[Re] => BareSchema[A]): Registry[(BareSchema[A], Re)] =
+    def addEntry[A](mkEntry: Registry[Re] => Schema[A]): Registry[(Schema[A], Re)] =
       new Registry((mkEntry(self), registry))
   }
 
-  type Id[A]       = A
-  type FromPers[A] = BareSchema[Person] => A
+  object Registry {
+    val empty: Registry[Unit] = new Registry(())
 
-  trait Domain {
+  }
 
-    def mkUser =
+  trait Provides[A] {
+    type T[Re] = Lookup[Re, A]
+  }
+
+  trait EList[T]
+
+  final case class ENil[T]() extends EList[T] {
+    def :/: [H[_]](head: H[T]): ECons[H, ENil, T] = ECons(head, this)
+  }
+  final case class ECons[H[_], T[x] <: EList[x], A](head: H[A], tail: T[A]) extends EList[A] {
+    def :/: [G[_]](elem: G[A]): ECons[G, ECons[H, T, ?], A] = ECons(elem, this)
+  }
+
+  object EList {
+    implicit def emptyEList[A]: ENil[A] = new ENil[A]()
+
+    implicit def consEList[A, H[A], T[A] <: EList[A]](
+      implicit head: H[A],
+      tail: T[A]
+    ): ECons[H, T, A] = ECons(head, tail)
+
+    implicit def selectHead[TC[_], T[x] <: EList[x], A](implicit list: ECons[TC, T, A]): TC[A] =
+      list.head
+
+//    implicit def selectTail[TC[_], ]
+  }
+
+  val mkUser =
+    (_: Unit) =>
       record(
         "active".narrow -*>: prim(JsonSchema.JsonBool) :*: "username".narrow -*>: prim(
           JsonSchema.JsonString
@@ -71,59 +103,59 @@ object module extends JsonModule[JsonSchema.type] with HasMigration[JsonSchema.t
         Iso[(Boolean, String), User]((User.apply _).tupled)(u => (u.active, u.username))
       )
 
-    val mkAdmin = record(
-      "username".narrow -*>: prim(JsonSchema.JsonString) :*: "rights".narrow -*>: seq(
-        prim(JsonSchema.JsonString)
-      ),
-      Iso[(String, List[String]), Admin]((Admin.apply _).tupled)(a => (a.username, a.rights))
-    )
+  val uv1 = mkUser.andThen(_.addField("active".narrow, false))
 
-    def mkRole[Re: Registry](implicit u: Lookup[Re, User], a: Lookup[Re, Admin]) =
-      (r: Registry[Re]) =>
-        union(
-          "user".narrow -+>: r.lookup[User] :+:
-            "admin".narrow -+>: r.lookup[Admin],
-          Iso[User \/ Admin, Role] {
-            case -\/(u) => u
-            case \/-(a) => a
-          } {
-            case u @ User(_, _)  => -\/(u)
-            case a @ Admin(_, _) => \/-(a)
-          }
+  val mkAdmin = record(
+    "username".narrow -*>: prim(JsonSchema.JsonString) :*: "rights".narrow -*>: seq(
+      prim(JsonSchema.JsonString)
+    ),
+    Iso[(String, List[String]), Admin]((Admin.apply _).tupled)(a => (a.username, a.rights))
+  )
+  /*
+  def mkRole[Re: Provides[User]#T: Provides[Admin]#T] =
+    new ForAll[WithRegistry[Role, ?], ECons[Provides[User]#T, ECons[Provides[Admin]#T, ENil, ?], ?]] {
+
+      def apply[Re: ECons[Provides[User]#T, ECons[Provides[Admin]#T, ENil, ?], ?]] =
+        (r: Registry[Re]) =>
+          union(
+            "user".narrow -+>: r.lookup[User] :+:
+              "admin".narrow -+>: r.lookup[Admin],
+            Iso[User \/ Admin, Role] {
+              case -\/(u) => u
+              case \/-(a) => a
+            } {
+              case u @ User(_, _)  => -\/(u)
+              case a @ Admin(_, _) => \/-(a)
+            }
+          )
+    }
+
+   */
+  type WithRegistry[A, Re] = Registry[Re] => Schema[A]
+
+  def mkPerson = new ForAll[WithRegistry[Person, ?], Provides[Role]#T] {
+
+    def apply[Re: Provides[Role]#T]: WithRegistry[Person, Re] =
+      (reg: Registry[Re]) =>
+        record(
+          "name".narrow -*>: prim(JsonSchema.JsonString) :*:
+            "role".narrow -*>: optional(
+            reg.lookup[Role]
+          ),
+          Iso[(String, Option[Role]), Person]((Person.apply _).tupled)(p => (p.name, p.role))
         )
-
-    def mkPerson[Re](reg: Registry[Re])(implicit r: Lookup[Re, Role]) =
-      record(
-        "name".narrow -*>: prim(JsonSchema.JsonString) :*:
-          "role".narrow -*>: optional(
-          reg.lookup[Role]
-        ),
-        Iso[(String, Option[Role]), Person]((Person.apply _).tupled)(p => (p.name, p.role))
-      )
-
   }
+  /*
+  val version2 = Registry.empty
+    .addEntry(_ => mkUser)
+    .addEntry(_ => mkAdmin)
+    .addEntry(mkRole)
+    .addEntry(mkPerson)
 
-  trait Version2 extends Domain {
+  def userV1 = mkUser.addField("active".narrow, false)
 
-    type Types = (BareSchema[Admin], (BareSchema[User], Unit))
-
-    implicit val registry: Registry[Types] = new Registry(())
-      .addEntry(_ => mkUser)
-      .addEntry(_ => mkAdmin)
-
-    val withRole = registry.addEntry(mkRole[Types])
-
-  }
-
-  object V2 extends Version2
-
-  trait Version1 extends Version2 {
-
-    val reg = withRole.addEntry(_ => userV1)
-
-    def userV1 = mkUser.addField("active".narrow, false)
-  }
-
-  object V1 extends Version1 {}
-
+  val version1 = version2
+    .addEntry(_ => userV1)
+//    .addEntry(mkRole)
+ */
 }
