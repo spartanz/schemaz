4cacfaf0cce6029924de30a574a0274ead611521
Valentin Kasas <valentin.kasas@gmail.com>
2019-09-19 22:05:21 +0200

    Committing WIP so that friends can see what I'm up to

--- modules/core/src/main/scala/Versioning.scala
+++ modules/core/src/main/scala/Versioning.scala
@@ -1,7 +1,7 @@
 package schemaz
 
 trait Versioning[R <: Realisation] extends SchemaModule[R] {
-
+  /*
   final val Current: Version[Unit, Unit] = new Version(())
 
   sealed case class Version[Types, Re](registry: Re)(
@@ -326,5 +326,5 @@ trait Versioning[R <: Realisation] extends SchemaModule[R] {
     }
 
   }
-
+ */
 }
